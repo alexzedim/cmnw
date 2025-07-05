fa4487bf428dbf1df1ccd58c8e3ef39405495c6a
Sasha <alexzedim@gmail.com>
2025-07-06 00:00:16 +0300

    refactor: auctions logs to market

--- apps/market/src/main.ts
+++ apps/market/src/main.ts
@@ -5,7 +5,7 @@ import { APP_LABELS } from '@app/resources';
 
 async function bootstrap() {
   const app = await NestFactory.create(MarketModule);
-  app.useLogger(new LoggerService(APP_LABELS.A));
+  app.useLogger(new LoggerService(APP_LABELS.M));
   await app.listen(3002);
 }
 bootstrap();
