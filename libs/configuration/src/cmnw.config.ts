f9daf23dea3a1ca273596393ac1959b4a8c91e1a
Sasha <alexzedim@gmail.com>
2025-10-24 15:22:48 +0300

    feat: remove MongoDB from API and standardize OAuth env vars to BATTLENET_*

--- libs/configuration/src/cmnw.config.ts
+++ libs/configuration/src/cmnw.config.ts
@@ -1,9 +1,9 @@
 import { ICmnwConfig } from '@app/configuration/interfaces';
 
 export const cmnwConfig: ICmnwConfig = {
-  clientId: process.env.CMNW_CLIENT_ID,
-  clientSecret: process.env.CMNW_CLIENT_SECRET,
-  redirectUri: process.env.CMNW_REDIRECT_URL,
+  clientId: process.env.BATTLENET_CLIENT_ID,
+  clientSecret: process.env.BATTLENET_CLIENT_SECRET,
+  redirectUri: process.env.BATTLENET_CALLBACK_URL,
   port: Number(process.env.CMNW_PORT),
   origin: [process.env.CMNW_ORIGIN],
 };
