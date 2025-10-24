d71f7f9ad2785945ba67f0166a9264bfa4bba16f
Sasha <alexzedim@gmail.com>
2025-10-24 15:39:39 +0300

    refactor: remove unused config properties and clean up environment variables

--- libs/configuration/src/core.config.ts
+++ libs/configuration/src/core.config.ts
@@ -1,6 +1,5 @@
 import { ICoreConfig } from '@app/configuration/interfaces';
 
 export const coreConfig: ICoreConfig = {
-  path: process.env.KEYS_PATH,
   useProxy: process.env.KEYS_USE_PROXY === 'true',
 };
--- libs/configuration/src/interfaces/cmnw.interface.ts
+++ libs/configuration/src/interfaces/cmnw.interface.ts
@@ -1,7 +1,3 @@
 export interface ICmnwConfig {
-  readonly clientId: string;
-  readonly clientSecret: string;
-  readonly redirectUri: string;
   readonly port: number;
-  readonly origin: string[];
 }
