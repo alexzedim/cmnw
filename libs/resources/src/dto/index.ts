46f962840e90df3aad982d4847fd4e109050d639
Sasha <alexzedim@gmail.com>
2025-12-28 14:10:00 +0300

    Add unified /dma/item/chart endpoint with data transformation

--- libs/resources/src/dto/index.ts
+++ libs/resources/src/dto/index.ts
@@ -1,5 +1,6 @@
 export * from './item-cross-realm.dto';
 export * from './item-chart.dto';
+export * from './item-chart-response.dto';
 export * from './item-feed.dto';
 export * from './item-quotes.dto';
 export * from './wow-token.dto';
---
+++ libs/resources/src/dto/item-chart-response.dto.ts
@@ -0,0 +1,44 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IChartOrder } from '@app/resources';
+
+export class ItemChartResponseDto {
+  @ApiProperty({
+    name: 'yAxis',
+    type: () => [String],
+    isArray: true,
+    description: 'Price levels formatted as strings for display',
+    example: ['1.00', '1.50', '2.00', '2.50'],
+  })
+  readonly yAxis: string[];
+
+  @ApiProperty({
+    name: 'xAxis',
+    isArray: true,
+    description: 'Timestamps as numbers (milliseconds since epoch)',
+    example: [1703752800000, 1703839200000, 1703925600000],
+  })
+  readonly xAxis: number[];
+
+  @ApiProperty({
+    name: 'dataset',
+    type: () => Object,
+    isArray: true,
+    description: 'Array of chart data points with market information',
+    example: [
+      {
+        x: 0,
+        y: 0,
+        value: 100,
+        orders: 5,
+        oi: 500,
+      },
+    ],
+  })
+  readonly dataset: Array<{
+    x: number;
+    y: number;
+    value: number;
+    orders: number;
+    oi: number;
+  }>;
+}
