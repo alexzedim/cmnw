3820c050f9075bd69cafde08b4a2ce02432699b1
Sasha <alexzedim@gmail.com>
2026-03-09 19:29:20 +0300

    refactor(resources): merge key-pool service into blizzard-api service

--- libs/resources/src/services/index.ts
+++ libs/resources/src/services/index.ts
@@ -1,5 +1,3 @@
 // Note: realms-cache.service is NOT exported here to avoid circular dependencies with TypeORM.
 // Import directly from './services/realms-cache.service' if needed.
-export * from './worker-metrics.service';
 export * from './blizzard-api.service';
-export * from './key-pool.service';
