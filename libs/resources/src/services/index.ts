d7ff0fb7686d47e0d3db1d7f9bb60bedf8a1f345
Sasha <alexzedim@gmail.com>
2026-03-21 22:54:45 +0300

    refactor(resources): remove old BlizzardApiService

--- libs/resources/src/services/index.ts
+++
@@ -1,4 +0,0 @@
-// Note: realms-cache.service is NOT exported here to avoid circular dependencies with TypeORM.
-// Import directly from './services/realms-cache.service' if needed.
-export * from './blizzard-api.service';
-export * from './blizzard-api-client';
