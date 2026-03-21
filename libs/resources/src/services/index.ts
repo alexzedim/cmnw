6679d0b743553a3cb0fa694b6a327156b3548883
Sasha <alexzedim@gmail.com>
2026-03-21 21:42:56 +0300

    feat(blizzard-api): Introduce `BlizzardApiClient` abstraction

--- libs/resources/src/services/index.ts
+++ libs/resources/src/services/index.ts
@@ -1,3 +1,4 @@
 // Note: realms-cache.service is NOT exported here to avoid circular dependencies with TypeORM.
 // Import directly from './services/realms-cache.service' if needed.
 export * from './blizzard-api.service';
+export * from './blizzard-api-client';
